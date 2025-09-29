b5896c533c1f813ac3d9ffd2fa39280176c07446
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-09-29 14:45:37 +0000

    Create feature component

---
+++ components/wc-feature-item.js
@@ -0,0 +1,63 @@
+const style = /*html*/ ``;
+
+class WcFeatureItem extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    console.log("wc-feature-item connected");
+    const children = this.innerHTML;
+
+    // Only insert template if not already rendered
+    if (!this.querySelector("template")) {
+      this.innerHTML = /*html*/ `
+        <template>
+          <style>
+            wc-feature-item {
+              display: block;
+              
+              article {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: calc((24 / 16) * 1em);
+                
+                .number {
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  font-family: var(--font-family-display); 
+                  font-weight: var(--font-weight-semibold);
+                  font-size: calc((20 / 16) * 1rem);
+                  line-height: calc((40 / 16) * 1rem);
+                  border: 1px solid;
+                  border-radius: 50%;
+                  width: 2.75em;
+                  aspect-ratio: 1 / 1;
+                }
+
+                p {
+                  text-align: center;
+                  line-height: 180%;
+                }
+              }
+            }
+          </style>
+          <article>
+            ${children}
+          </article>
+        </template>`;
+    }
+
+    // Grab the template inside the element itself
+    const template = this.querySelector("template");
+    const clone = template.content.cloneNode(true);
+
+    // Clear and append
+    this.innerHTML = "";
+    this.appendChild(clone);
+  }
+}
+
+customElements.define("wc-feature-item", WcFeatureItem);
